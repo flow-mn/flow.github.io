df41c3c7d0e70651a47547d6f20c2c9f1b4926c4
Batmend Ganbaatar <batmend@gege.mn>
2025-01-05 23:44:33 +0800

    cname

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
-<h3>Flow.</h3>
-<h4>a free and open-source personal finance tracking app</h4>
+<h4>Flow - a free and open-source personal finance tracking app</h4>
 
 <p>
 	(written by ai, accurate enough, i'll write one myself later) Flow is a free and open-source
@@ -7,6 +6,20 @@
 	complete set of features to help track your finance. Flow is built with privacy in mind and does
 	not require you to create an account or share your data with anyone.
 </p>
+
+<h4>Download Flow:</h4>
+
+<ul>
+	<li>
+		<a
+			href="https://play.google.com/store/apps/details?id=mn.flow.flow?utm_source=web"
+			target="_blank">Google Play Store</a
+		>
+	</li>
+	<li>
+		<a href="https://testflight.apple.com/join/NH4ifijS" target="_blank">TestFlight (beta)</a>
+	</li>
+</ul>
 
 <style>
 	* {
