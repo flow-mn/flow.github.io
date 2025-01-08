72db7f16526380969a26d772f4d070d9ba803f0e
Batmend Ganbaatar <batmend@gege.mn>
2025-01-08 16:09:23 +0800

    fix google play link, add image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,20 +7,28 @@
 	not require you to create an account or share your data with anyone.
 </p>
 
+<br />
+
 <h4>Download Flow:</h4>
 
-<ul>
+<ul data-no-bullet>
 	<li>
 		<a
-			href="https://play.google.com/store/apps/details?id=mn.flow.flow?utm_source=web"
-			target="_blank">Google Play Store</a
+			href="https://play.google.com/store/apps/details?id=mn.flow.flow&utm_source=web"
+			target="_blank"
+			aria-label="Google Play Store"
 		>
+			<img src="/googleplaystore.svg" alt="Google Play Store Download Button" />
+		</a>
 	</li>
 	<li>
 		<a
 			href="https://apps.apple.com/mn/app/flow-expense-tracker/id6477741670?utm_source=web"
-			target="_blank">App Store</a
+			target="_blank"
+			aria-label="App Store"
 		>
+			<img src="/appstore.svg" alt="App Store Download Button" />
+		</a>
 	</li>
 </ul>
 
