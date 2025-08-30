0df2954bff9f316ff455982b75250ceb4b82cb2a
Batmend Ganbaatar <batmend@gege.mn>
2025-08-30 13:55:55 +0800

    docs init

---
+++ docs/astro.config.mjs
@@ -0,0 +1,34 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Flow',
+      defaultLocale: 'en',
+      locales: {
+        en: {
+          label: 'English',
+        },
+        // mn: {
+        //   label: 'Монгол',
+        // },
+      },
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/flow-mn/flow' }],
+			sidebar: [
+				{
+					label: 'Guides',
+					autogenerate: { directory: 'guides' },
+				}
+			],
+			customCss: ['./src/styles/global.css'],
+		}),
+	],
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	base: '/docs'
+});
